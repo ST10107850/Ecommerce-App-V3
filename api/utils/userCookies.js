b9a753300ed5b4e7afdc7611b46f72f2b48a859c
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-02 19:05:57 +0200

    removing all other files

--- api/utils/userCookies.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.clearAuthCookies = void 0;
-const clearAuthCookies = (res) => {
-    res.clearCookie("jwt");
-    res.clearCookie("refreshToken");
-};
-exports.clearAuthCookies = clearAuthCookies;
-//# sourceMappingURL=userCookies.js.map
