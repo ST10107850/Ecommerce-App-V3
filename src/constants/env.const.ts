46f909df1bd6b536d0a4bd87bc7ca0e3b10823c0
ST10107850 <ST10107850@rcconnect.edu.za>
2025-04-02 18:48:00 +0200

    Adding vercel file

--- src/constants/env.const.ts
+++ src/constants/env.const.ts
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const MONGO_URI = process.env.MONGO_URI as string;
-export const PORT = process.env.PORT || (5000 as number);
+export const PORT = process.env.PORT || (3000 as number);
 export const JWT_SECRET = process.env.JWT_SECRET as string;
 export const NODE_ENV = process.env.NODE_ENV as string;
 
--- src/server.ts
+++ src/server.ts
@@ -10,19 +10,27 @@ import cartRoute from "./routes/cartRoute";
 import orderRoute from "./routes/orderRoute";
 
 const app = express();
-app.use(cookieParser());
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cookieParser());
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-app.use("/api/users", userRoute);
-app.use("/api/category", categoryRoute);
-app.use("/api/product", productRoute);
-app.use("/api/cart", cartRoute);
-app.use('/api/orders', orderRoute)
+// app.use("/api/users", userRoute);
+// app.use("/api/category", categoryRoute);
+// app.use("/api/product", productRoute);
+// app.use("/api/cart", cartRoute);
+// app.use("/api/orders", orderRoute);
 
-app.use(notFound);
-app.use(errorHandle);
-app.listen(PORT, () => {
+// app.use(notFound);
+// app.use(errorHandle);
+
+app.get("/", (req, res)=>{
+  res.send({message: "Hello world"})
+})
+
+
+app.listen(Number(PORT), () => {
   console.log("App is running on port: ", PORT);
-  dbConnection();
+  // dbConnection();
 });
+
+export default app;
